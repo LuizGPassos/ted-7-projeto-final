d1a48242c03e9f516bead9c59cc02beaab2c3c7d
luiz.souza <lzgustavo.passos@gmail.com>
2025-03-13 18:54:51 -0300

    first_data_ingestion_code

--- api-requests/api_data.py
+++ api-requests/api_data.py
@@ -1,8 +1,10 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
+from datetime import datetime
 
-url = "https://api.themoviedb.org/3/authentication"
+url = "https://api.themoviedb.org/3/discover/movie"
 
 load_dotenv()
 
@@ -15,6 +17,19 @@ headers = {
     "Authorization": auth
 }
 
+today = datetime.now().strftime("%Y-%m-%d")
+
 response = requests.get(url, headers=headers)
 
-print(response.text)
+data = response.json()
+
+for movie in data["results"]:
+    movie["ingestion_date"] = today
+
+movies = data["results"]
+    
+ 
+
+with open("./dataset/movie_dataset.json", "w") as file:
+    json.dump(movies, file, indent=4)
+
